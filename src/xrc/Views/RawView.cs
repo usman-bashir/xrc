8c8ad6930bcf01159437e0fa1492d8f367b60705
Davide Icardi <davide.icardi@gmail.com>
2012-10-29 01:44:12 +0100

    Added RawView

---
+++ src/xrc/Views/RawView.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace xrc.Views
+{
+	public class RawView : IView
+    {
+        public byte[] Content
+        {
+            get;
+            set;
+        }
+
+		public string ContentType
+		{
+			get;
+			set;
+		}
+
+        public void Execute(IContext context)
+        {
+            if (Content == null)
+                throw new ArgumentNullException("Content");
+
+			if (ContentType != null)
+				context.Response.ContentType = ContentType;
+
+			// TODO Valutare se implementare dai metodi utilizzabili anche per content molto grandi (buffer?) 
+			// o se leggere direttamente file senza passare da byte[]
+            context.Response.OutputStream.Write(Content, 0, Content.Length);
+        }
+    }
+}
