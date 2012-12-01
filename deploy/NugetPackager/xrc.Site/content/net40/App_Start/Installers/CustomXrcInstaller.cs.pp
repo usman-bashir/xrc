b6cd4f7b91a6158ce695add50635e7504f2d0270
Davide Icardi <davide.icardi@gmail.com>
2012-12-02 00:59:26 +0100

    updated nuget package

---
+++ deploy/NugetPackager/xrc.Site/content/net40/App_Start/Installers/CustomXrcInstaller.cs.pp
@@ -0,0 +1,16 @@
+﻿using Castle.MicroKernel.Registration;
+
+namespace $rootnamespace$.Installers
+{
+    public class CustomXrcInstaller : IWindsorInstaller
+    {
+		public CustomXrcInstaller()
+		{
+		}
+
+        public void Install(Castle.Windsor.IWindsorContainer container, Castle.MicroKernel.SubSystems.Configuration.IConfigurationStore store)
+        {
+			// TODO Add your components here
+		}
+    }
+}
